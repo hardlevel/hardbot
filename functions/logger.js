a3bedf9da79c50b7e4727aeffb43f76357f80f28
Rafael Tuneca <rtuneca@grupodisal.com.br>
2023-12-21 16:57:11 -0300

    new test

--- functions/logger.js
+++ functions/logger.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
 const util = require('util');
+const path = require('path');
 
-const fileLog = fs.createWriteStream(baseDir + '/logs/server.log', {flags : 'w'});
-const ErrorLog = fs.createWriteStream(baseDir + '/logs/error.log', {flags : 'w'});
+const logsDir = path.join(baseDir + '/logs');
+
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir, { recursive: true });
+    console.log('Directory created:', logsDir);
+}
+    else {
+    console.log("Directory exists!")
+}
+
+const fileLog = fs.createWriteStream(logsDir + '/server.log', {flags : 'w'});
+const ErrorLog = fs.createWriteStream(logsDir + '/error.log', {flags : 'w'});
 const logOutput = process.stdout;
 
 

2c6f7ce3d261cfbf20b74d89657d451120ce3349
Rafael Tuneca <rtuneca@grupodisal.com.br>
2024-01-10 13:37:42 -0300

    changed scripts on package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,5 +4,7 @@ module.exports = {
     script : "./index.js",
     instances: 1,
     ignore_watch : ["node_modules", "logs", "dados.json","data","database"],
+    time: true,
+    log_date_format: "“DD-MM-YYYY HH:mm Z”"
   }]
 }
